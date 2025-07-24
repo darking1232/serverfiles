775a54f505ccf199f41d23021fb09cbc101215a2
darking1232 <glemorjoaquin@gmail.com>
2025-07-24 17:34:32 +0800

    Add files via upload

---
+++ src/custom/defines_pre.hpp
@@ -0,0 +1,17 @@
+// Copyright (c) rAthena Dev Teams - Licensed under GNU GPL
+// For more information, see LICENCE in the main folder
+
+#ifndef CONFIG_CUSTOM_DEFINES_PRE_HPP
+#define CONFIG_CUSTOM_DEFINES_PRE_HPP
+
+/**
+ * rAthena configuration file (http://rathena.org)
+ * For detailed guidance on these check http://rathena.org/wiki/SRC/config/
+ **/
+
+#define PACKETVER 20151104
+//#define PACKET_OBFUSCATION_KEY1 0x4c17382a
+//#define PACKET_OBFUSCATION_KEY2 0x7ed174c9
+//#define PACKET_OBFUSCATION_KEY3 0x29961e4f
+
+#endif /* CONFIG_CUSTOM_DEFINES_PRE_HPP */
